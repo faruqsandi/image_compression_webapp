9de4d519eec8a328154ecc559b2c6853209c98e6
Faruq Sandi <faruqsandi@gmail.com>
2024-07-31 01:58:49 +0700

    remove useless stuff

--- celery_app.py
+++ celery_app.py
@@ -25,22 +25,17 @@ app.conf.task_default_routing_key = "direct"  # type: ignore
 app.conf.result_extended = True
 
 
-class Result(BaseModel):
-    file_path: str
-    old_filesize: int
-    new_filesize: int
-
-
 @app.task(queue="tnr_imgcmprs_queue", name="compress_image")
 def compress_image(file_path: str, quality: int = 50):
     img = Image.open(file_path)
     old_filesize = os.path.getsize(file_path)
     img.save(file_path, quality=quality)
     new_filesize = os.path.getsize(file_path)
-    result = Result(
-        file_path=file_path, old_filesize=old_filesize, new_filesize=new_filesize
-    )
-    return result.model_dump()
+    return {
+        "file_path": file_path,
+        "old_filesize": old_filesize,
+        "new_filesize": new_filesize,
+    }
 
 
 @app.task(queue="tnr_imgcmprs_queue", name="delete_task_result")
