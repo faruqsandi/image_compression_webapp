d2d29d06c789b5bd3546af25d71cd8a4cf382184
Faruq Sandi <faruqsandi@gmail.com>
2024-07-31 01:41:14 +0700

    working

--- celery_app.py
+++ celery_app.py
@@ -1,5 +1,8 @@
-from celery import Celery
+import os
 
+from celery import Celery, signals
+from PIL import Image
+from pydantic import BaseModel
 
 app = Celery(
     main="tnr_imgcmprs",
@@ -17,13 +20,9 @@ app.conf.task_queues = {
 }
 
 app.conf.task_default_queue = "tnr_imgcmprs_queue"
-app.conf.task_default_exchange = "compressor" # type: ignore
-app.conf.task_default_routing_key = "direct" # type: ignore
-
-
-from PIL import Image
-import os
-from pydantic import BaseModel
+app.conf.task_default_exchange = "compressor"  # type: ignore
+app.conf.task_default_routing_key = "direct"  # type: ignore
+app.conf.result_extended = True
 
 
 class Result(BaseModel):
@@ -42,3 +41,35 @@ def compress_image(file_path: str, quality: int = 50):
         file_path=file_path, old_filesize=old_filesize, new_filesize=new_filesize
     )
     return result.model_dump()
+
+
+@app.task(queue="tnr_imgcmprs_queue", name="delete_task_result")
+def delete_task_result(task_id):
+    result = app.AsyncResult(task_id)
+    file_path, *_ = result.args
+    if file_path and os.path.isfile(file_path):
+        os.remove(file_path)
+    result.forget()
+
+
+@app.task(queue="tnr_imgcmprs_queue", name="schedule_deletion")
+def schedule_deletion(task_id, countdown: int):
+    delete_task_result.apply_async((task_id,), countdown=countdown)  # type: ignore
+
+
+@signals.task_success.connect(sender=compress_image)
+def task_success_handler(sender, result, **kwargs):
+    task_id = sender.request.correlation_id
+    schedule_deletion.delay(task_id, 60 * 30)  # type: ignore
+
+
+@signals.task_failure.connect(sender=compress_image)
+def task_failure_handler(sender, task_id, **kwargs):
+    task_id = task_id
+    schedule_deletion.delay(task_id, 0)  # type: ignore
+
+
+@signals.task_revoked.connect(sender=compress_image)
+def task_revoked_handler(sender, request, **kwargs):
+    task_id = request.id
+    schedule_deletion.delay(task_id, 0)  # type: ignore
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
+import uuid
 from typing import Optional, Union
 
 import aiofiles
 import filetype
 from celery.exceptions import CeleryError
 from fastapi import FastAPI, UploadFile
-from fastapi.responses import RedirectResponse
+from fastapi.responses import FileResponse, RedirectResponse
 from pydantic import BaseModel
 
 from celery_app import app as celery_app
@@ -42,7 +43,10 @@ class TaskOut(BaseModel):
 
 @app.post("/upload")
 async def upload(file: UploadFile):
-    temporary_file_path = f"{TEMP_DIR}/{file.filename}"
+    random_uuid = uuid.uuid4().hex
+
+    temporary_file_path = f"{TEMP_DIR}/{random_uuid}_{file.filename}"
+    os.makedirs(os.path.dirname(temporary_file_path), exist_ok=True)
     async with aiofiles.open(temporary_file_path, "wb") as buffer:
         data = await file.read()
         await buffer.write(data)
@@ -50,9 +54,40 @@ async def upload(file: UploadFile):
     if not filetype.helpers.is_image(temporary_file_path):
         return ErrorResponseSchema(message="File is not an image", code=400)
 
-    task = compress_image.delay(temporary_file_path)  # type: ignore
+    task = compress_image.delay(temporary_file_path, 50)  # type: ignore
     return ResponseSchema(
         data={"task_id": task.id}, message="File uploaded successfully"
+    )
+
+
+@app.get("/download/{task_id}")
+def download(task_id: str):
+    task = celery_app.AsyncResult(task_id)
+    if task.status == "SUCCESS":
+        result = task.result
+        if result is None:
+            return ErrorResponseSchema(
+                message="File is already deleted, please try to upload again", code=500
+            )
+
+        file_path = result["file_path"]
+
+        if not os.path.isfile(file_path):
+            return ErrorResponseSchema(
+                message="File is not exist, please try to upload again.", code=400
+            )
+
+        kind = filetype.guess(file_path)
+        media_type = "application/octet-stream"
+        if kind:
+            media_type = kind.mime
+        file_name = os.path.basename(file_path)
+        file_name = file_name.split("_", 1)[1]
+        return FileResponse(
+            file_path, media_type=media_type, filename=os.path.basename(file_path)
+        )
+    return ErrorResponseSchema(
+        message="File is not ready for download yet. Please try again later", code=400
     )
 
 
